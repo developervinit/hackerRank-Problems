2541ce241669b606cd9a7ab177bffd207868b608
devvinit <vinit91.dev@gmail.com>
2023-03-17 20:00:08 +0530

    sparse arrays problem

--- 4_matchingString.js
+++ 4_matchingString.js
@@ -8,13 +8,14 @@
 //There are 2 instances of 'ab', 1 of 'abc' and 0 of 'bc'. For each query, add an element to the return array, result=[2, 1, 0].
 
 
-//steps to solve the problem.
+//logic-1 steps to solve the problem.
 //1. we will have for-loop according queries-array length.
 //2. have "count" variable initialised with 0;
 //3. have empty array variable.
 //4. then we will have nested loop according strings-array length.
 //5. in nested loop we will check if nestedloop[i] === parentloop[i] then increase the counter varibale and then outside of nestedloop and inside of parent loop counter variable will be pushed into an array then counter variable will be reset to 0.
 
+//Note:- this logic passes all test cases
 function matchingString(strings, queries){
     let count = 0;
     let occurenceInArray = [];
@@ -32,13 +33,46 @@ function matchingString(strings, queries){
 }
 
 //input
-strings = ["ab", "ab", "abc"];
-queries = ["ab", "abc", "bc"];
+let strings = ["ab", "ab", "abc"];
+let queries = ["ab", "abc", "bc"];
 
 //input
-strings1 = ["def", "de", "fgh"];
-queries2 = ["de", "lmn", "fgh"];
+let strings1 = ["def", "de", "fgh"];
+let queries1 = ["de", "lmn", "fgh"];
 
 
-console.log(matchingString(strings, queries)); //[ 2, 1, 0 ]
-console.log(matchingString(strings1, queries2)); //[ 1, 0, 1 ]
+// console.log(matchingString(strings, queries)); //[ 2, 1, 0 ]
+// console.log(matchingString(strings1, queries1)); //[ 1, 0, 1 ]
+
+
+//----------------------------------------------------------------------------
+
+
+//logic-2 and steps to solve the problem
+//running forEach on queries array.
+//getting length of strings array 
+//we will remove element which is common in both strings and queries arrays from strings array by using filter method on strings array.
+//now we have new strings array which is not having that perticular element. then we have length of it.
+//so we have new length of string array.
+//now we substract old array length from new array length and push that into an array. 
+
+//this logic dont pass all test cases
+function matchingString1(strings, queries){
+    let totalOccurance = [];
+    queries.forEach(query => {
+        let oldLength = strings.length;
+        strings = strings.filter(string => string !== query);
+        let newLength = strings.length;
+        let count = oldLength - newLength;
+        totalOccurance.push(count);
+    });
+    return totalOccurance;
+}
+
+let strings2 = [1000, "fzzgoijwadso", "jlnoyrcwzfkewbm", "obxmypenglkzgsykyb", "jlnoyrcwzfkewbm", "jlnoyrcwzfkewbm", "obxmypenglkzgsykyb"];
+let queries2 = [1000, "fzzgoijwadso", "jlnoyrcwzfkewbm"];
+
+// console.log(matchingString1(strings, queries));
+console.log(matchingString1(strings, queries)); 
+
+//link - https://programs.programmingoneonone.com/2021/05/hackerrank-sparse-arrays-solution.html
